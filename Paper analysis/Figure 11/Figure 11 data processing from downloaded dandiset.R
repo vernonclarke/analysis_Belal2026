ecc93456f9bcd437977a38c0b5d0c6d7346acfff
Vernon <149691755+vernonclarke@users.noreply.github.com>
2026-06-03 14:11:36 -0500

    update

--- Paper analysis/Figure 11/Figure 11 data processing from downloaded dandiset.R
+++ Paper analysis/Figure 11/Figure 11 data processing from downloaded dandiset.R
@@ -16,9 +16,6 @@ load_required_packages(c('jsonlite', 'reticulate', 'readABF', 'yaml'))
 env_name <- 'NWBenv'
 if (!env_name %in% reticulate::conda_list()$name) {
   reticulate::conda_create(env_name, python_version = '3.11')
-}
-installed_packages <- reticulate::py_list_packages(envname = env_name, type = 'conda')$package
-if (length(setdiff(c('pip', 'pynwb', 'numpy'), installed_packages))) {
   reticulate::conda_install(env_name, packages = c('pip', 'pynwb', 'numpy'), channel = 'conda-forge')
 }
 reticulate::use_condaenv(env_name, required = TRUE)
